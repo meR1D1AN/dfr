71e84ef91e49c33cc137ae7be9fb572fbb0758ca
Nikita <nikita@mer1d1an.ru>
2024-07-14 18:28:56 +0300

    Решение задачи №4

--- users/serializers.py
+++ users/serializers.py
@@ -1,9 +1,17 @@
 from rest_framework import serializers
 
-from users.models import User
+from users.models import User, Payment
+
+
+class PaymentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Payment
+        fields = '__all__'
 
 
 class UserSerializer(serializers.ModelSerializer):
+    payments = PaymentSerializer(many=True, read_only=True)
+
     class Meta:
         model = User
-        fields = ["id", "email", "phone", "city", "avatar"]
+        fields = ['id', 'email', 'phone', 'city', 'avatar', 'payments']
