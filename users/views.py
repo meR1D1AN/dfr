01a06e49802766ca0b2090e51aefaea5616e01b3
Никита Шидогубов <156647259+meR1D1AN@users.noreply.github.com>
2024-07-24 10:59:37 +0300

    Update views.py

--- users/views.py
+++ users/views.py
@@ -15,6 +15,12 @@ class UserCreateView(CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = (AllowAny,)
+    
+    
+    def perform_create(self, serializer):
+        user = serializer.save(is_active=True)
+        user.set_password(user.password)
+        user.save()
 
 
 class UserRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
