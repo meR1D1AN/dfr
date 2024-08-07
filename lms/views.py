4a7a170307d490e2c1047e5fb7b3837b30ec6d56
Nikita <nikita@mer1d1an.ru>
2024-08-07 19:12:56 +0300

    Решение второй задачи на скринкасте

--- lms/views.py
+++ lms/views.py
@@ -1,17 +1,20 @@
 from rest_framework import generics, viewsets
 
 from lms.models import Course, Lesson
+from lms.paginations import CustomPagination
 from lms.serializers import CourseSerializer, LessonSerializer
 
 
 class CourseViewSet(viewsets.ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
+    pagination_class = CustomPagination
 
 
 class LessonListCreateAPIView(generics.ListCreateAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    pagination_class = CustomPagination
 
 
 class LessonRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
