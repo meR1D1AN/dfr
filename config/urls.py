115b94a697eccae16fd9063b88e4339a44ed70b1
Nikita <nikita@mer1d1an.ru>
2024-08-10 11:51:12 +0300

    Решение задачи №1

--- config/urls.py
+++ config/urls.py
@@ -2,9 +2,25 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="LMS API",
+        default_version="v1",
+        description="API for LMS",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("lms.urls", namespace="lms")),
     path("users/", include("users.urls", namespace="users")),
+    path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema_swagger_ui"),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema_redoc"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
