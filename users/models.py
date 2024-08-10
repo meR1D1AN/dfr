112abaaf36ba0e0c9a8065458bfd288ae87ad7f9
Nikita <nikita@mer1d1an.ru>
2024-08-10 10:56:27 +0300

    Задание №2 выполнено по скринкасту

--- users/models.py
+++ users/models.py
@@ -88,3 +88,37 @@ class Payment(models.Model):
     class Meta:
         verbose_name = "Платёж"
         verbose_name_plural = "Платёжи"
+
+
+class Donat(models.Model):
+    amount = models.PositiveIntegerField(
+        verbose_name="Сумма пожертвования",
+        help_text="Укажите сумму пожертвования",
+    )
+    session_id = models.CharField(
+        max_length=255,
+        verbose_name="ID сессии",
+        help_text="Укажите ID сессии",
+        **NULLABLE
+    )
+    link = models.URLField(
+        max_length=400,
+        verbose_name="Ссылка на оплату",
+        help_text="Укажите ссылку на оплату",
+        **NULLABLE
+    )
+    user_link = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        related_name="donats",
+        verbose_name="Пользователь",
+        help_text="Укажите пользователя",
+        **NULLABLE
+    )
+
+    def __str__(self):
+        return f"Пожертвование на сумму {self.amount} руб."
+
+    class Meta:
+        verbose_name = "Пожертвование"
+        verbose_name_plural = "Пожертвования"
